274295b3bc32988c1993640addd4311360a81a56
JaeHyeon <nitsss15@gmail.com>
2023-01-11 21:18:46 +0900

    Initial commit

---
+++ src/main/java/com/ayoh/coreshop/CoreShopApplication.java
@@ -0,0 +1,13 @@
+package com.ayoh.coreshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreShopApplication.class, args);
+	}
+
+}
